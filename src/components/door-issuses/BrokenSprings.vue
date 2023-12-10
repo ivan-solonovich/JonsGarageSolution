9547253c3dac2c10f86b040cc4f0f4c4dff0dc12
ivan-solonovich <solonovichivan1978@gmail.com>
2023-12-10 08:29:05 +0300

    first commit

--- src/components/door-issuses/BrokenSprings.vue
+++ src/components/door-issuses/BrokenSprings.vue
@@ -3,7 +3,22 @@
 </script>
 
 <template>
-<div>Broken springs</div>
+  <div class="w-100 ">
+    <h1 class="mb-4 text-3xl font-extrabold  md:text-5xl lg:text-6xl text-center text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Broken springs</h1>
+<div class="   p-10 m-10">
+
+
+        <p>A garage door spring balances the weight of a garage door and enables easy movement - opening and closing manually or
+        automatically. It is made of high-tension steel, which has a limited lifespan and loses its effectiveness with time.
+        Garage door springs have different quantity levels such as ‘10,000-use’ or ‘75,000-use’. Many buyers feel that this
+        is quite a large number but when you evaluate and analyze, you will realize that a garage door is probably opened four
+        to five times a day, every day, every year justifying the high quantity levels.</p>
+        <p>How do you know that you need a garage door spring repair?</p>
+        <p>As your garage door ages, it becomes less effective. The steel loses resiliency and the door begins to weigh more.
+          When springs are brand new, you just need around 10 pounds of force to lift and open the door. But when springs move
+          towards the end of their lifespan, owners need to apply more force to lift the door, almost as much as the door’s weight</p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
--- src/components/navbar/NavBar.vue
+++ src/components/navbar/NavBar.vue
@@ -29,6 +29,7 @@ export default {
         console.log("Desktop", deskTop)
       }
     } )
+
   },
   methods: {
     isPopUpVisible(){
