2c67c070ac3e03c104570892b2bde60aeb5ee39f
ivan-solonovich <solonovichivan1978@gmail.com>
2024-02-12 18:37:10 +0300

    first commit

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,6 +1,7 @@
 <script>
 import AppointModal from "./appoint-modal/AppointModal.vue";
 import OurPartners from "./our-partners/OurPartners.vue";
+import Features from "./features/features.vue";
 export default {
   data() {
     return {
@@ -9,6 +10,7 @@ export default {
     }
   },
   components:{
+    Features,
     OurPartners,
     AppointModal
   },
@@ -119,7 +121,20 @@ export default {
 
     />
   </header>
+  <features/>
+  <h3 class="w-full text-center m-2 p-2 text-orange-600" >Call us now</h3>
+  <div  class="w-full text-center  p-2">
+    <a class="phone" href="tel:+18183177803">
+      <button  class="bg-green-800 hover:bg-gray-400 rounded-[.5rem] text-black-800  py-2 px-4 rounded inline-flex items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+        </svg>
+        <span> +1 818 317 78 03</span>
+      </button>
+    </a>
+  </div>
   <OurPartners/>
+
 </template>
 
 <style scoped>
--- src/components/our-partners/OurPartners.vue
+++ src/components/our-partners/OurPartners.vue
@@ -12,7 +12,10 @@
       <img class="partner_img" src="../../assets/partners/p_3.jpg" alt="">
     </div>
   </div>
-  <h3 class="mb-4 mt-4 text-2xl text-center">Most common parts for repairs</h3>
+  <h3 class="mb-4 mt-4 text-2xl text-center">Professional Garage Door Repair</h3>
+  <div class="w-full flex flex-wrap justify-center text-center m-2 p-2 ">
+    <h4>Trouble Opening or Closing, Blocked Sensors, Dying Opener, Unusual Noises, Uneven Door, Slow Opening, Transmitter Problems, Keypad Not Synced and other problems</h4>
+  </div>
   <div class="w-100 flex flex-direction-column columns-1 justify-center">
 
     <div class="w-100 p-10 m-10 flex flex-wrap justify-center gap-8">
@@ -23,24 +26,38 @@
       <img class="partner_img" src="../../assets/shemes/5.jpg" alt="">
     </div>
   </div>
-  <h3 class="mb-4 mt-4 text-2xl text-center">Professional Garage Door Repair</h3>
-  <div class="w-full flex flex-wrap justify-center text-center m-2 p-2 ">
-    Trouble Opening or Closing, Blocked Sensors, Dying Opener, Unusual Noises, Uneven Door, Slow Opening, Transmitter Problems, Keypad Not Synced and other problems
+
+
+  <div class="w-full  container mx-auto p-10">
+    <h3 class="text-2xl">Do your garage doors exhibit any of the following:</h3>
+    <ul class="points list-disc" style="margin-bottom: 15px; margin-top: 15px;">
+      <li style="margin-left: 20px;">Grinding/rubbing Noises: If your garage door makes strange sounds when opening or closing, the doors could have warped (if wooden) or twisted if electrically operated.</li>
+      <li style="margin-left: 20px;">Door jammed: Wooden doors will swell in damp, cold, or wet conditions preventing smooth opening and closing.</li>
+      <li style="margin-left: 20px;">Jammed solid: This happens when the door comes off the track/runners.</li>
+      <li style="margin-left: 20px;">Rapid Closing: The timing sequence needs adjusting and timed to perfection that could compromise safety or cause vehicle damage.</li>
+      <li style="margin-left: 20px;">Slow Opening or Closing: A sluggish or slow action can make you late for that important appointment.</li>
+      <li style="margin-left: 20px;">Door Won’t Stay Open: The doors open but then begin closing of their own accord.</li>
+      <li style="margin-left: 20px;">Sensor Problems: Remote sensor is not activating the motor.</li>
+      <li style="margin-left: 20px;">Damaged Hinges: Worn or loose hinges can cause a door to drop and jam.</li>
+      <li style="margin-left: 20px;">Old and/or Damaged Door/Track Rollers: Preventing a smooth rolling action over the tracking, causing them to jam.</li>
+      <li style="margin-left: 20px;">Faulty Locks: Unsecure garage/roller doors can be a thoroughfare for burglars into a property.</li>
+    </ul>
+    <p>Our team of garage door specialists in Los Angeles will put right all the above issues and many more not listed. Call us if you are experiencing any problems with your garage doors.</p>
   </div>
-  <div class="w-full flex col-auto justify-center">
-  <ul class="m-2 p-10 list-disc">
-    <li>Broken or bent rollers</li>
-    <li>Bent track, misalignment or rusted track</li>
-    <li>Preventative Maintenance</li>
-    <li>Others services</li>
-  </ul>
-  <ul class="m-2 p-10 list-disc">
-    <li>Replace garage door opener chain</li>
-    <li>Replace garage door spring</li>
-    <li>Replace garage door section</li>
-    <li>Replace garage door opener motor</li>
-  </ul>
-  </div>
+<!--  <div class="w-full flex col-auto justify-center">-->
+<!--  <ul class="m-2 p-10 list-disc">-->
+<!--    <li>Broken or bent rollers</li>-->
+<!--    <li>Bent track, misalignment or rusted track</li>-->
+<!--    <li>Preventative Maintenance</li>-->
+<!--    <li>Others services</li>-->
+<!--  </ul>-->
+<!--  <ul class="m-2 p-10 list-disc">-->
+<!--    <li>Replace garage door opener chain</li>-->
+<!--    <li>Replace garage door spring</li>-->
+<!--    <li>Replace garage door section</li>-->
+<!--    <li>Replace garage door opener motor</li>-->
+<!--  </ul>-->
+<!--  </div>-->
   <h3 class="w-full text-center m-2 p-2 text-orange-600" >Call us now</h3>
   <div  class="w-full text-center  p-2">
     <a class="phone" href="tel:+18183177803">
