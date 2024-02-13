13cb11006f2a0952223c18faff1a26a76f559408
ivan-solonovich <solonovichivan1978@gmail.com>
2024-02-13 18:38:51 +0300

    first commit

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -111,9 +111,10 @@ export default {
           <slot></slot>
         </div>
     </div>
-    <video  autoplay loop muted class="absolute z-10 w-auto min-w-full min-h-full max-w-none bg-opacity-50" >
-      <source src="https://staging.streamable.com/0dluc3" type="video/mp4" />Your browser does not support the video tag.
 
+    <video  autoplay loop muted class="absolute z-10 w-auto min-w-full min-h-full max-w-none bg-opacity-50" >
+      <source src="../../matireal/frame1compresed.mp4" type="video/mp4">Your browser does not support the video tag.
+      <source src="../../matireal/frame1compresed.webm" type="video/webm">Your browser does not support the video tag.
     </video>
 <!--  Pop Up component    -->
     <AppointModal
@@ -131,7 +132,7 @@ export default {
     <h3 class="w-full flex flex-wrap justify-center m-2 p-2 text-orange-600" >Call us now</h3>
     <div  class="w-full flex justify-center  p-2">
       <a class="phone" href="tel:+18183177803">
-        <button  class="bg-green-800 hover:bg-gray-400 rounded-[.5rem] text-black-800   py-2 px-4 rounded inline-flex items-center">
+        <button  class="bg-green-800 hover:bg-gray-400 rounded-[.5rem] text-white   py-2 px-4 rounded inline-flex items-center">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mp-2 pr-2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
           </svg>
