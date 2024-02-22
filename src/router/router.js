7c2efd1968e539e9f866e7a3d23ec79da4463510
ivan-solonovich <solonovichivan1978@gmail.com>
2024-02-22 20:16:52 +0300

    first commit

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHashHistory} from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 import MainPage from "../components/MainPage.vue";
 import AboutPage from "../components/AboutPage.vue";
 import ContactPage from "../components/ContactPage.vue";
@@ -70,7 +70,7 @@ const routes =
 
 const router = createRouter({
 
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes,
     scrollBehavior(to, from, savedPosition) {
         if (savedPosition) {
